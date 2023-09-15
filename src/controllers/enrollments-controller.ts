f9c5520888b670229d66d0daa4fe3a1d515b20ca
Quillinan <milton.quillinan@gmail.com>
2023-09-15 17:25:34 -0300

    first commit

---
+++ src/controllers/enrollments-controller.ts
@@ -0,0 +1,36 @@
+import { Response } from 'express';
+import httpStatus from 'http-status';
+import { AuthenticatedRequest } from '@/middlewares';
+import { enrollmentsService } from '@/services';
+import { cepValidationSchema } from '@/schemas';
+
+export async function getEnrollmentByUser(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+
+  const enrollmentWithAddress = await enrollmentsService.getOneWithAddressByUserId(userId);
+
+  return res.status(httpStatus.OK).send(enrollmentWithAddress);
+}
+
+export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, res: Response) {
+  await enrollmentsService.createOrUpdateEnrollmentWithAddress({
+    ...req.body,
+    userId: req.userId,
+  });
+
+  return res.sendStatus(httpStatus.OK);
+}
+
+// TODO - Receber o CEP do usuário por query params.
+export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
+  const { cep } = req.query;
+
+  const { error } = cepValidationSchema.validate(cep);
+
+  if (error) {
+    return res.status(httpStatus.BAD_REQUEST).json({ error: 'CEP inválido' });
+  }
+
+  const address = await enrollmentsService.getAddressFromCEP(cep as string);
+  res.status(httpStatus.OK).send(address);
+}
