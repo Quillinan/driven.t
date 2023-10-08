2d61d03e7f941fcdd9927fd33143ca07cf344460
Quillinan <milton.quillinan1798@gmail.com>
2023-10-08 15:34:30 -0300

    delete

--- src/controllers/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './users-controller';
-export * from './authentication-controller';
-export * from './events-controller';
-export * from './enrollments-controller';
-export * from './tickets-controller';
-export * from './payments-controller';
-export * from './hotels-controller';
